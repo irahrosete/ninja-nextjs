9c83141fcdcadb3a2e06e5021e32fbea30095b91
irah rosete <irah.rosete@outlook.com>
2022-03-19 20:20:37 +1100

    add global and scope styles

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
     return ( 
-        <div>
+        <footer>
             Copyright 2022 Ninja List
-        </div>
+        </footer>
      )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,11 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1>Homepage</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, excepturi quae, aspernatur rerum possimus odit laudantium ut error vero magni, hic omnis consectetur eveniet blanditiis. Molestias corporis nostrum nulla quaerat?</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, excepturi quae, aspernatur rerum possimus odit laudantium ut error vero magni, hic omnis consectetur eveniet blanditiis. Molestias corporis nostrum nulla quaerat?</p>
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, excepturi quae, aspernatur rerum possimus odit laudantium ut error vero magni, hic omnis consectetur eveniet blanditiis. Molestias corporis nostrum nulla quaerat?</p>
+      <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, excepturi quae, aspernatur rerum possimus odit laudantium ut error vero magni, hic omnis consectetur eveniet blanditiis. Molestias corporis nostrum nulla quaerat?</p>
       <Link href='/ninjas'>
-        <a>See Ninja Listing</a>
+        <a className={styles.btn}>See Ninja Listing</a>
       </Link>
     </div>
   )
