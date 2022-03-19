4b0c254722104f5af77015c885e6a91404a46b57
irah rosete <irah.rosete@outlook.com>
2022-03-19 19:22:24 +1100

    add navbar

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, excepturi quae, aspernatur rerum possimus odit laudantium ut error vero magni, hic omnis consectetur eveniet blanditiis. Molestias corporis nostrum nulla quaerat?</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, excepturi quae, aspernatur rerum possimus odit laudantium ut error vero magni, hic omnis consectetur eveniet blanditiis. Molestias corporis nostrum nulla quaerat?</p>
