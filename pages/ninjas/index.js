9da465491c4fa871216c3b855fe941f7c2b8a825
irah rosete <irah.rosete@outlook.com>
2022-03-20 10:29:44 +1100

    apply styles

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,3 +1,5 @@
+import styles from '../../styles/Ninjas.module.css'
+
 export const getStaticProps = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await res.json()
@@ -12,7 +14,7 @@ const Ninjas = ({ ninjas }) => {
             <h1>All Ninjas</h1>
             {ninjas.map(ninja => (
                 <div key={ninja.id}>
-                    <a>
+                    <a className={styles.single}>
                         <h3>{ninja.name}</h3>
                     </a>
                 </div>
