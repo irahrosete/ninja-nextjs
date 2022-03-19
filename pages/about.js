7422f7086254951c413fde0fa346a0262a495a25
irah rosete <irah.rosete@outlook.com>
2022-03-19 18:23:25 +1100

    update index and add about page

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+  return (
+    <div>
+        <h1>About</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia velit reiciendis, nesciunt numquam inventore facere nemo ipsa quo eum minus. Suscipit earum obcaecati aut esse harum ipsa doloremque quas impedit?</p>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officia velit reiciendis, nesciunt numquam inventore facere nemo ipsa quo eum minus. Suscipit earum obcaecati aut esse harum ipsa doloremque quas impedit?</p>
+    </div>
+  )
+}
+
+export default About
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div>
-      <h1>HOMEPAGE</h1>
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, excepturi quae, aspernatur rerum possimus odit laudantium ut error vero magni, hic omnis consectetur eveniet blanditiis. Molestias corporis nostrum nulla quaerat?</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, excepturi quae, aspernatur rerum possimus odit laudantium ut error vero magni, hic omnis consectetur eveniet blanditiis. Molestias corporis nostrum nulla quaerat?</p>
     </div>
   )
 }
