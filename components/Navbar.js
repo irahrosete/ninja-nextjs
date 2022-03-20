7666726f1a2dfc67a79722ba2fe539e18aa7b667
irah rosete <irah.rosete@outlook.com>
2022-03-20 11:00:55 +1100

    fix links

--- components/Navbar.js
+++ components/Navbar.js
@@ -9,7 +9,7 @@ const Navbar = () => {
             </div>
             <Link href='/'><a>Home</a></Link>
             <Link href ='/about'><a>About</a></Link>
-            <Link href='ninjas'><a>Ninja Listing</a></Link>
+            <Link href='/ninjas'><a>Ninja Listing</a></Link>
         </nav>
     )
 }
